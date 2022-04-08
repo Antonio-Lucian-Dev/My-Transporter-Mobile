417b37a3131dce2438a4022c8572332a4f04fd2f
AsuSoftware <asu.software.dev@gmail.com>
2022-04-08 16:42:34 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'My Transporter',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
